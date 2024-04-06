2abe66ee53725c0897f9b65b5394f0cbec289a7c
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-04-06 14:44:30 +0100

    Allow user to view tasks with drop down for details

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,5 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	// select initialisation
 	let selects = document.querySelectorAll("select");
 	M.FormSelect.init(selects);
+
+	let collapsibles = document.querySelectorAll(".collapsible");
+	M.Collapsible.init(collapsibles);
 });
 
